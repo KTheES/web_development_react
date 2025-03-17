2525af838b11aca454121cbcb345be201374ac8c
KTheES <keunsu2889@gmail.com>
2025-03-17 19:33:47 +0900

    feat: 20250317 kpoplist / contextAPI review started

--- 01-starting-project/src/App5.jsx
+++ 01-starting-project/src/App5.jsx
@@ -8,38 +8,12 @@ import { EXAMPLES } from "./data_updated";
 
 function App() {
 
-  const [selectedTopic, setSelectedTopic] = useState();
-  // 특정 상태를 업데이트해서 컴포넌트 렌더링을 일으키는 useState 단계로 넘어가고 있다.
+  const [selectedTopic, setSelectedTopic] = useState('');
   
   function handleSelect(selectedTopic) {
     console.log(selectedTopic);
     setSelectedTopic(selectedTopic);
   }
-
-  // React에서는 변수의 데이터에 html 코드를 삽입할 수 있음.
-  let tabContent = (
-    <>
-      <p>Please Select a Topic👾</p>
-    </>
-  );
-
-  // 버튼을 클릭했을 때 해당 내용을 참조하여 내용이 출력되어야 함.
-
-  if(selectedTopic) { // selectedTopic에 값이 무엇이라도 있기만 하면 이하의 코드가 실행됨
-    tabContent = (
-      <>
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>
-            {EXAMPLES[selectedTopic].code}
-          </code>
-        </pre>
-      </>
-    );
-  }
-
-
 
   return (
     <div>
@@ -66,9 +40,18 @@ function App() {
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
             {/* ↑children */}
           </menu>
-            <div id="tab-content">
-          {tabContent}
-            </div>
+          {selectedTopic === '' ? <h3>버튼을 클릭하세요</h3> :          
+          <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+              {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
+          }        
+
         </section>
       </main>
     </div>
