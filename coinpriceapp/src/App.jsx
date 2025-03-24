fe4201a139ef1a0e41163c1a3bde37c41b45da9c
KTheES <keunsu2889@gmail.com>
2025-03-24 20:11:37 +0900

    feat: movie app started

--- coinpriceapp/src/App.jsx
+++ coinpriceapp/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  // setLoading(false);를 적용하게 되는 시점이 언제인가?
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')   // API 받아옴
+    .then(response => response.json())
+    // .then(json => console.log(json));
+    .then(json => {
+      setCoins(json);
+      setLoading(false);
+    });
+    // async always가 더 낫다
+  }, []);    //[] : 의존성 배열, 안에 아무것도 작성하지 않았을 시 "아무것도 참조하지 않는다"는 뜻.
+  console.log(coins);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+        <h1>Coin Price Tracker</h1>
+      {loading ? (<strong>Loading . . . </strong>) : (null)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
   )
 }
 
